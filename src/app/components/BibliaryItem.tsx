54e4de1251e480c7c3b20fcb51a4a2300c5bff2e
Alberto <abcelkuqui@gmail.com>
2024-01-05 23:21:58 -0500

    corrrect behavior for play playlists

--- src/app/components/BibliaryItem.tsx
+++ src/app/components/BibliaryItem.tsx
@@ -1,14 +1,15 @@
 import { Playlist } from "@/lib/data";
+import Link from "next/link";
 
 export default function BibliaryItem({playlist}:{playlist:Playlist}){
     const{id,albumId,artists,color,cover,title}=playlist
     return(
-        <li key={albumId} className="flex flex-nowrap gap-4 text-xs hover:bg-slate-300 hover:text-orange-700 ps-2 rounded  w-full h-10 truncate">
+        <Link href={`/${albumId}`} key={albumId} className="flex  gap-4 text-xs hover:bg-slate-300 hover:text-orange-700 p-2 ps-2 rounded  w-full  ">
             <img className="w-10 h-10 " src={cover} />
-            <div className="">
+            <div className="truncate">
                 <div>{title}</div>
                 <div>{artists}</div>
             </div>    
-        </li>
+        </Link>
     )
 }
